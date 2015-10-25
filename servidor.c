29f383b87f6c023eca818ae86321be3e787aec96
Cajjunb <Cajjunb@gmail.com>
2015-10-25 13:54:37 -0200

    primeira implementacao nova do round robin

--- servidor.c
+++ servidor.c
@@ -67,11 +67,12 @@ void dummy(int signal){}
 
 void imprimeLista(t_processo **cabeca){
     t_processo *aux = *cabeca;
+    unsigned int j = 0;
     while(aux != NULL){
-    	if (aux->pid.size() > 0)
+    	for (j = 0; j < count; ++j)
     	{
-			printf("\n\tProcesso\t%u\t deltaHora %u:%u x %u\t minstamp\t%u\n"
-						  ,aux->pid.back()
+			printf("\tProcesso\t%u\t deltaHora %u:%u x %u\t minstamp\t%u\n"
+						  ,aux->pid[j]
 						  ,aux->deltaHora
 						  ,aux->deltaMin
 						  ,aux->vezes
@@ -96,10 +97,12 @@ int main(){
 	int ppid;					// Parent Process ID
 	int i;
 	unsigned int j;
+	int w;
 	
 	signal(SIGALRM,dummy);				//ROTINA DE SIGNAL_ALRM
 	key_msg = msgget(10,0x1FF);			//CRIAR FILA DE MSG
-	while(1){
+	while(1)
+	{
 		while(msgrcv(key_msg,&msgrecebida,size_msg,0,IPC_NOWAIT) > 0){			//LOOP DE RECEBER MENSAGENS E INSERIR NA FILA PROCESSSOS
 			insere_lista(&cabeca,&msgrecebida);
 		}	
@@ -109,51 +112,76 @@ int main(){
 			time(&tempoCorrente);
 			tempoInfo = localtime(&tempoCorrente); 					//PEGA O TEMPO ATUAL DE FORMA SEPARADA
 			if( aux->minstamp == (unsigned) ((tempoInfo->tm_hour*60) + tempoInfo->tm_min) && aux->vezes > 0  ){
-				printf("\n\tHORA DE EXECUTAR UM PROCESSO!\n");
+				printf("\tHORA DE EXECUTAR UM PROCESSO!\n");
 				ppid = fork();
-				printf("\n\tppid = %d \n",ppid);
+				// /*debug*/ printf("\tppid = %d \n",ppid);
 				if(ppid == 0){	
-					printf("end1\n");
+					// /*debug*/ printf("end1\n");
 					if(execl(aux->msg,aux->msg,EComercial, (char*)0) < 0){
-						printf("\nErro no execl! \n");
+						printf("Erro no execl! \n");
 						exit(1);
 					}
 				}else{
-					printf("end2\n");
+					// /*debug*/ printf("end2\n");
 					aux->vezes--;		
 					aux->minstamp = ((tempoInfo->tm_hour*60) + tempoInfo->tm_min) + aux->deltaHora*60 + aux->deltaMin  ; //ATUALIZAR A PROX VEZ
-					aux->pid.push_back( ppid) ;
-					printf("\n\telse aux->pid = %u \n",aux->pid.back());
+					
+					w=0;
+					while(aux->pid[w] != 0)
+					{
+						w++;
+					}
+					aux->pid[w] = ppid;
+					// /*debug*/ printf("\telse aux->pid = %u \n",aux->pid[w]);
 				}
-				printf("end\n");
+				// /*debug*/ printf("end\n");
 			}
 			tamanhoLista++;	
 			aux = aux->prox;
-			printf("\n\twhile(aux != NULL)FINAL\n");
+			// /*debug*/ printf("\twhile(aux != NULL)FINAL\n");
 		}
 		
 		
 		
-		printf("\n\tSAI DO LOOP DE EXEC\n");
+		// /*debug*/ printf("\tSAI DO LOOP DE EXEC\n");
 		imprimeLista(&cabeca);
-		printf("logo apos imprimir lista\n");
+		// /*debug*/ printf("logo apos imprimir lista\n");
 		
 		aux = cabeca;
-		if(aux != NULL){
+		if(aux != NULL)
+		{
+			printf("aux eh diferente de null!\n");
 			for( i = 0; i < tamanhoLista - 1; i++)
 			{
+				printf("i=%d, aux->pid.size()=%u\n", i, aux->pid.size());
 				for (j = 0; j < aux->pid.size(); ++j)
 				{
-					printf("\n\tProcesso\t%d\t deltaHora %u:%u x %u\t minstamp\t%u\tprox\t=%p\n"
-									,aux->pid[j]
-									,aux->deltaHora
-									,aux->deltaMin
-									,aux->vezes
-									,aux->minstamp
-									,aux->prox);
+					// printf("\tProcesso\t%d\t deltaHora %u:%u x %u\t minstamp\t%u\tprox\t=%p\n"
+					// 				,aux->pid[j]
+					// 				,aux->deltaHora
+					// 				,aux->deltaMin
+					// 				,aux->vezes
+					// 				,aux->minstamp
+					// 				,aux->prox);
 					
-					if(aux->vezes > 0 && (aux->pid[j] != 0) ){
-						kill(aux->pid.back(),SIGSTOP);
+					if(aux->pid[j] != 0)
+					{
+						/*debug*/ printf("1 mandei sigstop para aux pid[%u]=%d\n", j, aux->pid[j]);
+						kill(aux->pid[j],SIGSTOP);
+
+						//se ainda tenho pids na lista atual de processos filhos
+						if ( j < aux->pid.size() - 1)
+						{
+							/*debug*/ printf("2 (if) - mandei SIGCONT para aux pid[%u]=%d\n", j+1, aux->pid[j+1]);
+							kill(aux->pid[j+1],SIGCONT);
+						}
+						else	
+						{
+							/*debug*/ printf("3 (else)mandei SIGCONT para aux prox pid[0]=%d\n", aux->prox->pid[0]);
+							// primeiro do vetor seguinte. o processo que está na proxima ficah do vetor
+							kill(aux->prox->pid[0],SIGCONT);
+						}
+
 					}		
 					alarm(3);
 				}
